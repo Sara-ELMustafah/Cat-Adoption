91dcaaa49726e83a93ed082ab3ae3cceee04c026
Sara-ELMustafah <saraelmustafah@gmail.com>
2021-03-27 19:02:15 +0300

    Cat Adoption Task

---
+++ AdobtCatJavaScript.js
@@ -0,0 +1,36 @@
+﻿let col1 = [];
+let col2 = [];
+let col3 = [];
+let col4 = [];
+
+let rowNum = 1;
+let index = 0;
+
+function FormToTable() {
+
+    let insertedCats = document.getElementById('DisplayForm');
+    let newRow = insertedCats.insertRow(rowNum);
+
+    col1[index] = document.getElementById('tname').value;
+    col2[index] = document.getElementById('tbreed').value;
+    col3[index] = document.getElementById('tage').value;
+    col4[index] = document.getElementById('tcolor').value;
+
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    let cell3 = newRow.insertCell(2);
+    let cell4 = newRow.insertCell(3);
+
+    cell1.innerHTML = col1[index];
+    cell2.innerHTML = col2[index];
+    cell3.innerHTML = col3[index];
+    cell4.innerHTML = col4[index];
+
+    rowNum++;
+    index++;
+
+    document.getElementById('tname').value = "";
+    document.getElementById('tbreed').value = "";
+    document.getElementById('tage').value = "";
+    document.getElementById('tcolor').value = "";
+}
---
+++ HomeJavaScript.js
@@ -0,0 +1,21 @@
+﻿'use strict'
+fetch("https://api.thecatapi.com/v1/images/search?format=json&limit=12")
+    .then((response) => response.json())
+    .then((data) => {  
+        for (let i=0;i<12;i++) {
+            let catImage = data[i].url;
+            let catId = data[i].id;
+            let gallery = document.querySelector(".gallery");
+            let div = document.createElement("div");
+            gallery.appendChild(div);
+            div.classList.add("parent");
+            let eimg = document.createElement("img");            
+            let eh4 = document.createElement("h4");
+            eh4.innerHTML = "ID: " + `${catId}`;           
+            eimg.setAttribute('src', `${catImage}`);            
+            
+            div.appendChild(eimg);
+            div.appendChild(eh4);
+        }
+    })
+    .catch((e) => console.log(e));
